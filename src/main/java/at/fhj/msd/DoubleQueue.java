395e6e45fb6c77688d10cb1aa120cd834ef6983a
Jasmin Ramsbacher <jasmin.ramsbacher1@edu.fh-joanneum.at>
2025-04-29 00:17:32 +0200

    Fixed wrong pushes. Now ready to merge into Main (pom.xml)

--- src/main/java/at/fhj/msd/DoubleQueue.java
+++ src/main/java/at/fhj/msd/DoubleQueue.java
@@ -9,23 +9,10 @@ public class DoubleQueue implements IDoubleQueue {
     private final List<Double> elements = new ArrayList<Double>();
     private int maxSize = 5;
 
-
-    /**
-     * Constructor for DoubleQueue
-     * DoubleQueue implements the Interface IDoubleQueue which has methods declared for a Queue.
-     * This Class provides functionality to store and handle Double values in a queue.
-     * @param maxSize
-     */
     public DoubleQueue(int maxSize) {
         this.maxSize = maxSize;
     }
 
-    /**
-     * @param obj
-     * The  offer method is overridden and adds an double (obj) to the elements and retruns a boolean value if 
-     * the method worked. The Method adds an item if the Method is
-     * 
-     */
     @Override
     public boolean offer(Double obj) {
         if (elements.size() != maxSize) {

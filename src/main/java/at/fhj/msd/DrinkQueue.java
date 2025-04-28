743ec9a71d601c19c8d4f9cf950001fbbad42633
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-04-28 22:10:50 +0200

    Added Method offer and poll to the DrinkQueue.java
    The Rest of The Methods will be made by other contributors

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -4,30 +4,41 @@ import java.util.ArrayList;
 
 public class DrinkQueue implements IQueue {
 
-      private ArrayList<String> elements = new ArrayList<String>();
+      private ArrayList<Drink> elements = new ArrayList<Drink>();
       private int maxSize;
 
   public DrinkQueue(int maxSize) {
       this.maxSize = maxSize;
   }    
 
-  public boolean offer(String obj)
+  public boolean offer(Drink obj)
   {
-      
+      if (elements.size() < maxSize) {
+          elements.add(obj);
+          return true;
+      } else {
+          return false;
+      }
   }
 
 
       @Override
   public String poll()
   {
-
+      if (elements.isEmpty()) {
+              return null;
+      } else {
+          Drink firstElement = elements.get(0);
+          elements.remove(0);
+          return firstElement.toString();
+      }
   }
 
  
       @Override
   public String remove()
   {
-
+      
   }
 
    

72becf091da16243ff33cf62dcd3e4193d5e8278
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-04-28 22:01:48 +0200

    Added and Created Base Structure of DrinkQueue.java
    Needs Implementation for the different Methods

---
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -0,0 +1,45 @@
+package at.fhj.msd;
+
+import java.util.ArrayList;
+
+public class DrinkQueue implements IQueue {
+
+      private ArrayList<String> elements = new ArrayList<String>();
+      private int maxSize;
+
+  public DrinkQueue(int maxSize) {
+      this.maxSize = maxSize;
+  }    
+
+  public boolean offer(String obj)
+  {
+      
+  }
+
+
+      @Override
+  public String poll()
+  {
+
+  }
+
+ 
+      @Override
+  public String remove()
+  {
+
+  }
+
+   
+  public String peek()
+  {
+
+  }
+
+  
+  public String element()
+  {
+
+  }
+
+}

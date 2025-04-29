cc31f644ea422e4e540b5faf745f39d76c2529a3
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-04-29 21:07:53 +0200

    Implemented remove() and element() method inside DrinkQueue.java

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,60 +1,57 @@
 package at.fhj.msd;
 
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
-public class DrinkQueue  {
+public class DrinkQueue {
 
-      private LinkedList<Drink> elements;
-      int maxSize = 5;
+    private LinkedList<Drink> elements;
+    int maxSize = 5;
 
-  public DrinkQueue(int maxSize) {
-      elements = new LinkedList<>();
-      this.maxSize = maxSize;
-  }    
-
-  public boolean offer(Drink obj)
-  {
-    if(elements.size() > this.maxSize)
-     {
-      return false;
+    public DrinkQueue(int maxSize) {
+        elements = new LinkedList<>();
+        this.maxSize = maxSize;
     }
-    else
-    {
-      elements.add(obj);
-      return true;
+
+    public boolean offer(Drink obj) {
+        if (elements.size() > this.maxSize) {
+            return false;
+        } else {
+            elements.add(obj);
+            return true;
+        }
     }
-}
 
-      
-  public Drink poll()
-  {
-      return null;
-  }
-
- 
-     
-  public Drink remove()
-  {
-
-    return elements.remove();
-      
-  }
-
-   
-  public Drink peek()
-  {
-    if (!elements.isEmpty()) {
-      return null;
-    } else {
-      return elements.get(0);
+    public Drink poll() {
+        return null;
     }
-    
-  }
 
-  
-  public Drink element()
-  {
-    return null;
-  }
+    public Drink remove() {
+
+        Drink drink = poll();
+        if (drink == null) {
+            throw new NoSuchElementException("there's no element any more");
+        }
+
+        return drink;
+
+    }
+
+    public Drink peek() {
+        if (!elements.isEmpty()) {
+            return null;
+        } else {
+            return elements.get(0);
+        }
+
+    }
+
+    public Drink element() {
+        Drink drink = peek();
+        if (drink == null) {
+            throw new NoSuchElementException("there's no element any more");
+        }
+        return drink;
+    }
 
 }

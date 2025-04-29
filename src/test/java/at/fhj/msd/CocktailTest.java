30aafd543be864c8f77e8b5466ee5610176bed4c
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-04-29 22:28:53 +0200

    Made Test for Cocktail with 100% Covarage

---
+++ src/test/java/at/fhj/msd/CocktailTest.java
@@ -0,0 +1,75 @@
+package at.fhj.msd;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CocktailTest {
+      
+      @Test
+      @DisplayName("Test getVolume")
+      void testConstructor() {
+          Liquid l1 = new Liquid("L1", 1, 1);
+          Liquid l2 = new Liquid("L2", 1, 1);
+          Liquid l3 = new Liquid("L3", 1, 1);
+          ArrayList<Liquid> l = new ArrayList<Liquid>();
+          l.add(l1);
+          l.add(l2);
+          l.add(l3);
+          Cocktail c = new Cocktail("Anok", l);
+          double expected = 3.0;
+          double actual = c.getVolume();
+          assertEquals(expected, actual);
+      }
+
+      @Test
+      @DisplayName("Test getAlcoholPercent")
+      void testGetAlcoholPercent() {
+          Liquid l1 = new Liquid("L1", 1, 1);
+          Liquid l2 = new Liquid("L2", 1, 1);
+          Liquid l3 = new Liquid("L3", 1, 1);
+          ArrayList<Liquid> l = new ArrayList<Liquid>();
+          l.add(l1);
+          l.add(l2);
+          l.add(l3);
+          Cocktail c = new Cocktail("Anok", l);
+          double expected = 1.0;
+          double actual = c.getAlcoholPercent();
+          assertEquals(expected, actual);
+      }
+
+      @Test
+      @DisplayName("Test isAlcoholic")
+      void testIsAlcoholic() {
+          Liquid l1 = new Liquid("L1", 1, 1);
+          Liquid l2 = new Liquid("L2", 1, 1);
+          Liquid l3 = new Liquid("L3", 1, 1);
+          ArrayList<Liquid> l = new ArrayList<Liquid>();
+          l.add(l1);
+          l.add(l2);
+          l.add(l3);
+          Cocktail c = new Cocktail("Anok", l);
+          boolean expected = true;
+          boolean actual = c.isAlcoholic();
+          assertEquals(expected, actual);
+      }
+
+      @Test
+      @DisplayName("Test isNonAlcoholic")
+      void testIsNonAlcoholic() {
+          Liquid l1 = new Liquid("L1", 1, 0);
+          Liquid l2 = new Liquid("L2", 1, 0);
+          Liquid l3 = new Liquid("L3", 1, 0);
+          ArrayList<Liquid> l = new ArrayList<Liquid>();
+          l.add(l1);
+          l.add(l2);
+          l.add(l3);
+          Cocktail c = new Cocktail("Anok", l);
+          boolean expected = false;
+          boolean actual = c.isAlcoholic();
+          assertEquals(expected, actual);
+      }
+
+}
